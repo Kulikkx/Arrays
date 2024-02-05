4f07ce996533eb522e6b5a6b0e72f5962cc835a4
Andrey <Andrey@ANDREY>
2024-02-05 21:39:26 +0300

    introduction to Arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,4 +4,22 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, " ");
+	cout << "Hello Arrays" << endl;
+
+    int SIZE;
+	cout << "¬ведите размер массива";
+	cin >> SIZE;
+	int arr[SIZE];
+	cout << "¬ведите" << SIZE << "элементов массива";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+	cout<<"Ёлементы массива"
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
  }
