e44f560ab500325cc753615f144ba581a84680fa
Andrey <Andrey@ANDREY>
2024-02-25 20:51:21 +0300

    Arrays done

---
+++ Binary/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale (LC_ALL, "");
+	int decimal; //десятичное число
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 32;
+	bool bin[n] = {}; // этот массив будет хранить разряды двоичного числа
+#ifdef VAR_1
+	int i = 0;
+	while (decimal > 0)
+	{
+		bin[i] = decimal % 2; // получаем младший раряд двоичного числа
+		i++;// переходим к следующему разряду
+		decimal /= 2;  //убираем младший двоичный разряд из числа
+	}
+#endif // VAR_1
+	int i = 0;
+	for (; decimal; bin[i++] = decimal % 2, decimal /= 2);
+	for (--i; i >= 0; i--)
+	{
+		cout << bin[i];
+	}
+	cout << endl;
+}
